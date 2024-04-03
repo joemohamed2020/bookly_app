2ab08cc39a009004d174fbeefcf9b5496775f5e6
Yousef Mohamed <Y.Mohamed.fcai@gmail.com>
2024-04-03 04:48:36 +0200

    add animation to splash screen

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -2,24 +2,50 @@ import 'package:bookly_app/constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:get/get_navigation/src/routes/default_transitions.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<double> fadingAnimation;
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    fadingAnimation =
+        Tween<double>(begin: 0.0, end: 1.0).animate(animationController);
+    animationController.forward();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Image(
-          image: AssetImage(logo),
-        ),
-        Text(
-          "Browes More Books",
-          textAlign: TextAlign.center,
-        )
-      ],
-    );
+    return AnimatedBuilder(
+        animation: fadingAnimation,
+        builder: (context, _) {
+          return FadeTransition(
+            opacity: fadingAnimation,
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Image(
+                  image: AssetImage(logo),
+                ),
+                Text(
+                  "Browes More Books",
+                  textAlign: TextAlign.center,
+                )
+              ],
+            ),
+          );
+        });
   }
 }
