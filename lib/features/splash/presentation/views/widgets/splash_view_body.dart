2b5e1a4f3d8874fea14c8f725800c430dfd11061
Yousef Mohamed <Y.Mohamed.fcai@gmail.com>
2024-04-03 04:48:55 +0200

    dispose controller

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -26,6 +26,12 @@ class _SplashViewBodyState extends State<SplashViewBody>
   }
 
   @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
         animation: fadingAnimation,
