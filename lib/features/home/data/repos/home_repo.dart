86039c326ceb0aef2e5d3be08d618006f56c04ea
joemohamed2020 <y.mohamed.fcai@gmail.com>
2024-05-10 23:09:53 +0300

    create either class

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerListView();
-  fetchBooksListViewWidget();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerListView();
+  Future<Either<Failure, List<BookModel>>> fetchBooksListViewWidget();
 }
